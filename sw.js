d567f0a20cdc6185c330ce205057c7806d809305
jorgeemiliosarsfield-cloud <jorgeemiliosarsfield@gmail.com>
2026-08-15 03:15:26 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
-const CACHE="mcf-v5-3";
+const CACHE="mcf-v6-pin";
 self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
   if(e.request.mode==="navigate"){
-    e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{
-      const copy=r.clone(); caches.open(CACHE).then(c=>c.put("./index.html",copy)); return r;
-    }).catch(()=>caches.match("./index.html")));
-  } else {
+    e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{caches.open(CACHE).then(c=>c.put("./index.html",r.clone()));return r}).catch(()=>caches.match("./index.html")));
+  }else{
     e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
   }
 });
