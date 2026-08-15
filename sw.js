4fe796f37c4521a6ffbf6813fe56f776720ef1ff
jorgeemiliosarsfield-cloud <jorgeemiliosarsfield@gmail.com>
2026-08-15 03:36:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-const CACHE="mcf-v6-pin";
-self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
+const CACHE="mcf-v6-1";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin) return;
   if(e.request.mode==="navigate"){
-    e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{caches.open(CACHE).then(c=>c.put("./index.html",r.clone()));return r}).catch(()=>caches.match("./index.html")));
+    e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match("./index.html")));
   }else{
-    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
   }
 });
